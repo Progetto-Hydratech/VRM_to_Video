532c7673ca73fd1f2c7bc1980582b7a438f1dd61
Sebaf-26 <76589886+Sebaf-26@users.noreply.github.com>
2026-06-14 10:51:42 +0200

    fix: change ONVIF port to 8181

--- onvif_server.py
+++ onvif_server.py
@@ -11,7 +11,7 @@ HOST_IP   = os.environ.get('ONVIF_SERVER_IP', '192.168.1.112')
 RTSP_IP   = os.environ.get('RTSP_SERVER_IP',  '192.168.1.112')
 RTSP_PORT = os.environ.get('RTSP_PORT',        '8554')
 RTSP_PATH = os.environ.get('RTSP_PATH',        'victron')
-ONVIF_PORT= int(os.environ.get('ONVIF_PORT', '8090'))
+ONVIF_PORT= int(os.environ.get('ONVIF_PORT', '8181'))
 DEVICE_NAME=os.environ.get('DEVICE_NAME',      'Victron VRM')
 DEVICE_UUID= str(uuid4())
 
